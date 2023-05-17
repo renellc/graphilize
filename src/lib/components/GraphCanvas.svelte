ea12650bc63ff346ed7a7d1e0f2dbddbdf20ea31
Renell Castro <renellc9819@gmail.com>
2023-05-17 14:49:32 -0700

    Add label to each vertex

--- src/lib/components/GraphCanvas.svelte
+++ src/lib/components/GraphCanvas.svelte
@@ -9,24 +9,6 @@
 
   const circleRadius = 20;
 
-  const unsubUi = uiStore.subscribe((store) => {
-    if (!stage) {
-      return;
-    }
-
-    if (store.selectedControl === "drag") {
-      stage.container().style.cursor = "pointer";
-      stage.draggable(true);
-    } else {
-      stage.container().style.cursor = "default";
-      stage.draggable(false);
-    }
-  });
-
-  onDestroy(() => {
-    unsubUi();
-  });
-
   onMount(() => {
     stage = new Konva.Stage({
       container: "graphCanvasContainer",
@@ -41,6 +23,20 @@
     stage.on("touchend", addVertex);
     stage.on("wheel", handleZoom);
   });
+
+  function updateCursorStyle() {
+    if (!stage) {
+      return;
+    }
+
+    if ($uiStore.selectedControl === "drag") {
+      stage.container().style.cursor = "pointer";
+      stage.draggable(true);
+    } else {
+      stage.container().style.cursor = "default";
+      stage.draggable(false);
+    }
+  }
 
   function handleZoom(event: Konva.KonvaEventObject<WheelEvent>) {
     const oldScale = stage.scaleX();
@@ -79,19 +75,40 @@
     }
 
     for (let i = 0; i < $graphStore.vertices.length; i++) {
-      const vertex = $graphStore.vertices[i];
-      mainLayer.add(
-        new Konva.Circle({
-          fill: "blue",
-          radius: circleRadius,
-          x: vertex.x,
-          y: vertex.y,
-        })
-      );
+      const { x, y } = $graphStore.vertices[i];
+      const group = new Konva.Group({
+        x,
+        y,
+        width: circleRadius * 2,
+        height: circleRadius * 2,
+      });
+
+      const circle = new Konva.Circle({
+        fill: "white",
+        radius: circleRadius,
+        stroke: "#171717",
+        strokeWidth: 1,
+      });
+
+      const text = new Konva.Text({
+        text: i.toString(),
+        fill: "#171717",
+        fontSize: 16,
+        align: "center",
+        verticalAlign: "middle",
+      });
+
+      text.x(-text.width() / 2);
+      text.y(-text.height() / 2);
+
+      group.add(circle);
+      group.add(text);
+      mainLayer.add(group);
     }
   }
 
   $: $graphStore, drawVertices();
+  $: $uiStore, updateCursorStyle();
 </script>
 
 <div id="graphCanvasContainer" />
